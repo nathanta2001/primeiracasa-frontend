381bf9894e1dbb8f221f0780287de6c90e3f99b9
nathan <nathan.ta2001@gmail.com>
2026-04-07 16:30:58 -0300

    fix: ajustando a inteface pra desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import AppRoutes from './routes/AppRoutes';
-import useOnlineStatus from './hooks/useOnlineStatus';
-import Offline from './components/Offline';
 import InstallPrompt from './components/InstallPrompt';
 
 function App() {
-  const isOnline = useOnlineStatus();
-
-  if (!isOnline) {
-    return <Offline />;
-  }
-
-  return <>
-    <AppRoutes />
-    <InstallPrompt />
-  </>;
+  return (
+    <>
+      <AppRoutes />
+      <InstallPrompt />
+    </>
+  );
 }
 
 export default App;
