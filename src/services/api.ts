06594d657ed3e65d80dba7e52b3436fb13ab1989
nathan <nathan.ta2001@gmail.com>
2026-04-12 11:08:01 -0300

    chore: preparando o deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://primeiracasa-backend.onrender.com",
 });
 
 // Adiciona o token em cada chamada da API
